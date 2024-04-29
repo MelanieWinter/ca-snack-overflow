5069594a0b8a71d011f0648aa02d4f3214ac818d
Melanie Winter <melaniebucher830@gmail.com>
2024-04-29 05:21:43 -0700

    Im pretty sure i did something.. like an animation.. but it makes no sense.. im just saving it cause i dont trust my computer

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,13 +1,24 @@
-import { useNavigate } from 'react-router-dom'
-import Card from '../../components/Card/Card'
-import './Homepage.css'
+import { useNavigate } from 'react-router-dom';
+import Card from '../../components/Card/Card';
+import './Homepage.css';
 
 export default function Homepage() {
     const navigate = useNavigate(); 
-    const routeChange = () =>{ 
+    const routeChange = () => { 
         const path = `/game-room`; 
         navigate(path);
     }
+
+    // Array of card data (for example)
+    const cardData = [
+        { id: 1, title: 'Card 1' },
+        { id: 2, title: 'Card 2' },
+        { id: 3, title: 'Card 3' },
+        { id: 4, title: 'Card 4' },
+        { id: 5, title: 'Card 5' },
+        { id: 6, title: 'Card 6' },
+        { id: 7, title: 'Card 7' },
+    ];
 
     return (
         <section className="Homepage">
@@ -18,7 +29,11 @@ export default function Homepage() {
             >
                 Wanna Play?
             </button>
-            <Card />
+            <div className='cards-animation-div'>
+                {cardData.map(card => (
+                    <Card key={card.id} title={card.title} />
+                ))}
+            </div>
         </section>
-    )
+    );
 }
